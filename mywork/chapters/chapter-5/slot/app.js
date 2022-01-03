65b0974150c15231141a31d88f2a71343bfc29ae
sinigam1192 <yuie7893@gmail.com>
2022-01-03 18:59:03 +0900

    nameのslot

--- mywork/chapters/chapter-5/slot/app.js
+++ mywork/chapters/chapter-5/slot/app.js
@@ -8,10 +8,27 @@ var MyButton = {
     `
 }
 
+var MyPage = {
+    template: `
+    <div>
+        <header>
+            <slot name="header">header</slot>
+        </header>
+        <main>
+            <slot>body</slot>
+        </main>
+        <footer>
+            <slot name="footer">footer</slot>
+        </footer>
+    <div>
+    `
+}
+
 
 var vm = new Vue({
     el: '#app',
     components: {
         MyButton: MyButton,
+        MyPage: MyPage,
     }
 })
