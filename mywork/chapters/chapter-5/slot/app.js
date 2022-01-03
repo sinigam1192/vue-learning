626f795bf37999074a3577b864b181141fd95aee
sinigam1192 <yuie7893@gmail.com>
2022-01-03 20:46:22 +0900

    scopeのslot

--- mywork/chapters/chapter-5/slot/app.js
+++ mywork/chapters/chapter-5/slot/app.js
@@ -24,11 +24,60 @@ var MyPage = {
     `
 }
 
+var ScopeCheckMyButton = {
+    data: function(){
+        return {
+            textLabel: 'child'
+        }
+    },
+    template: `
+        <button>
+            <slot>OK</slot>
+        </button>
+    `
+}
+
+var ToDoList = {
+    props: {
+        todos: {
+            type: Array,
+            required: true,
+        }
+    },
+    template: `
+    <div>
+        <ul>
+            <template v-for="todo in todos">
+            <!-- v-bindディレクティブでtodoを親コンポーネントに渡す -->
+                <slot :todo="todo">
+                    <li :key="todo">
+                    {{ todo.text }}
+                    </li>
+                </slot>
+            </template>
+        </ul>
+    </div>
+    `
+}
+
 
 var vm = new Vue({
     el: '#app',
+    data: function(){
+        return {
+            textLabel: 'parent',
+            todos: [
+                {id: 1, text: "買い物", isCompleted: true},
+                {id: 2, text: "掃除", isCompleted: false},
+                {id: 3, text: "晩ご飯", isCompleted: true},
+                {id: 4, text: "洗濯", isCompleted: false}
+            ]
+        }
+    },
     components: {
         MyButton: MyButton,
         MyPage: MyPage,
+        ScopeCheckMyButton: ScopeCheckMyButton,
+        TodoList: ToDoList, 
     }
 })
