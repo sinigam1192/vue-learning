8a569ffaad53519e9af51def912445acf01c1a53
sinigam1192 <yuie7893@gmail.com>
2022-02-05 12:17:33 +0900

    mixinがなかったら

--- mywork/chapters/chapter-5/mix-in/app.js
+++ mywork/chapters/chapter-5/mix-in/app.js
@@ -1,1 +1,62 @@
 // #appにマウントしてください
+
+var IconShareButton = {
+    template: `
+    <button @click="share"><i class="fas fa-share-square"/></button>
+    `,
+    data: function() {
+        return {
+            _isProcessing: false,
+        }
+    },
+    methods: {
+        share: function() {
+            if(this._isProcessing){
+                return
+            }
+            if(!window.confirm('シェアしますか？')) {
+                return
+            }
+            this._isProcessing = true
+            setTimeout(() => {
+                window.alert('シェアしました')
+                this._isProcessing = false
+            }, 300)
+        }
+    }
+}
+
+var TextShareButton = {
+    template: `
+    <button @click="share">{{ buttonLabel }}</button> 
+    `,
+    data: function() {
+        return {
+            buttonLabel: 'シェアする',
+            _isProcessing: false,
+        }
+    },
+    methods: {
+        share: function() {
+            if(this._isProcessing){
+                return
+            }
+            if(!window.confirm('シェアしますか？')) {
+                return
+            }
+            this._isProcessing = true
+            setTimeout(() => {
+                window.alert('シェアしました')
+                this._isProcessing = false
+            }, 300)
+        }
+    }, 
+}
+
+new Vue({
+    el: '#app',
+    components: {
+        IconShareButton,
+        TextShareButton,
+    },
+})
