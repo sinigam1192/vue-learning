395a495a1a47ad9016f8118677fce98d06f88eb4
sinigam1192 <yuie7893@gmail.com>
2022-02-05 22:47:42 +0900

    cssのスコープ

---
+++ mywork/chapters/chapter-6/css-scoped/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p class="message">メッセージ:{{ msg }}</p>
+        <p>上記の要素にdata-vとスコープIDが付与される。</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: 'こんにちは!!!'
+        }
+    }
+}
+</script>
+
+// scopeでこの単一ファイル
+<style scoped>
+    .message { color: red; }
+</style>
